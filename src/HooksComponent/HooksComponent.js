288af677b313670d917b440710e7fc45cae5ab51
alexandr_li <alexandr_li@epam.com>
2019-10-17 15:55:18 +0600

    Changed examples

--- src/HooksComponent/HooksComponent.js
+++ src/HooksComponent/HooksComponent.js
@@ -2,13 +2,49 @@ import React, {useContext, useEffect, useState} from 'react';
 import {LocaleContext, ThemeContext} from '../context';
 import {Row} from '../shared/Row';
 
+/*
+function useInputValue(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  function handleValueChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {value, onChange: handleValueChange};
+}
+*/
+
+/*
+function useLogging(value) {
+  useEffect(() => {
+    console.log('Hooks component fullname: ', value);
+  }, [value]);
+}
+*/
+
+/*
+function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    console.log('useWindowWidth');
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return width;
+}
+*/
+
 export function HooksComponent() {
-  const name = useInputField('Alexandr');
-  const surname = useInputField('Li');
+  const name = useInputValue('Alexandr');
+  const surname = useInputValue('Li');
+  useLogging(name.value + ' ' + surname.value);
+
   const theme = useContext(ThemeContext);
   const locale = useContext(LocaleContext);
+
   const width = useWindowWidth();
-  useDocumentTitle(name.value + ' ' + surname.value);
 
   return (
     <section className={theme}>
@@ -24,30 +60,28 @@ export function HooksComponent() {
   );
 }
 
-function useInputField(initialValue) {
+function useInputValue(initialValue) {
   const [value, setValue] = useState(initialValue);
-
-  function handleChange(e) {
+  function handleValueChange(e) {
     setValue(e.target.value);
   }
 
-  return {value, onChange: handleChange};
+  return {value, onChange: handleValueChange};
 }
 
-function useDocumentTitle(title) {
+function useLogging(value) {
   useEffect(() => {
-    console.log('Hooks component fullname: ', title);
-  }, [title]);
+    console.log('Hooks component fullname: ', value);
+  }, [value]);
 }
 
 function useWindowWidth() {
   const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    console.log('useWindowWidth called');
+    console.log('useWindowWidth');
     const handleResize = () => setWidth(window.innerWidth);
     window.addEventListener('resize', handleResize);
-
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
